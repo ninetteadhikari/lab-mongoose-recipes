167e19f65f88a90437a67662619a45f0ccb958d2
Ninette <ninette.adhikari@gmail.com>
2019-09-06 13:30:46 +0200

    first pass

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe"); // Import of the model Recipe from './models/Recipe'
+const data = require("./data.js"); // Import of the data from './data.js'
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose
+  .connect("mongodb://localhost/recipeApp", {
+    useNewUrlParser: true
+  })
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
+// Recipe.create({data})
+
+// Recipe.findOne({
+//   title: "Carrot Cake"
+// }).then(data => {
+//   console.log(data)
+// });
+
+Recipe.create({ title: "NewCurry", cuisine: "Indian" }).then(data => {
+  console.log(data.title);
+});
+
+Recipe.insertMany(data).then(data => {
+  console.log(data.title);
+});
+
+Recipe.updateOne(
+  { name: "Rigatoni alla Genovese" },
+  { name: "Rigatoni alla Genovese", duration: 100 }
+).then(data => {
+  console.log("Recipe successfully updated");
+});
+
+Recipe.deleteOne({title:"Carrot Cake"}).then(data=>{
+  console.log("Recipe successfully deleted")
+})
